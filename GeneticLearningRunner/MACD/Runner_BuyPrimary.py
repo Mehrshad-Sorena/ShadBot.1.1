854cf89c9ca1d1fe575ba02f311d79d4987494cb
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-10-01 23:53:30 +0330

    Update Runner_BuyPrimary.py

--- GeneticLearningRunner/MACD/Runner_BuyPrimary.py
+++ GeneticLearningRunner/MACD/Runner_BuyPrimary.py
@@ -53,7 +53,7 @@ def Run():
 	optimizers.symbol = 'XAUUSD_i'
 	optimizers.sigpriority = 'primary'
 	optimizers.sigtype = 'buy'
-	optimizers.turn = 100
+	optimizers.turn = 1000
 	optimizers.dataset = parameters.elements['dataset_5M'].copy()
 	optimizers.timeframe = '5M'
 
