6fab05020c7aa8ac7a61bb585d422073b5d8d5b0
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-10-02 00:34:23 +0330

    Update Runner_SellSecondry.py

--- GeneticLearningRunner/StochAstic/Runner_SellSecondry.py
+++ GeneticLearningRunner/StochAstic/Runner_SellSecondry.py
@@ -50,7 +50,7 @@ def Run():
 	optimizers.symbol = 'XAUUSD_i'
 	optimizers.sigpriority = 'secondry'
 	optimizers.sigtype = 'sell'
-	optimizers.turn = 5000
+	optimizers.turn = 7000
 	optimizers.dataset = parameters.elements['dataset_5M'].copy()
 	optimizers.timeframe = '5M'
 
