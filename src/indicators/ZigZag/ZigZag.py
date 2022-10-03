290b5ecdddc34f57104b85941f93086dfa8ce2c1
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-10-03 20:21:53 +0330

    Create ZigZag.py

---
+++ src/indicators/ZigZag/ZigZag.py
@@ -0,0 +1,15 @@
+from zigzag import peak_valley_pivots
+import pandas as pd
+
+def Find(dataset, index_first, index_last):
+
+	ZigZag = peak_valley_pivots(
+								dataset['close'][index_first : index_last], 
+								abs(dataset['close'][index_first : index_last].pct_change(1)).mean(),
+								-abs(dataset['close'][index_first : index_last].pct_change(1)).mean()
+								)
+
+	ts_ZigZag = pd.Series(dataset['close'][index_first:index_last], index=dataset['close'][index_first : index_last].index)
+	ts_ZigZag = ts_ZigZag[ZigZag != 0]
+
+	return ts_ZigZag
