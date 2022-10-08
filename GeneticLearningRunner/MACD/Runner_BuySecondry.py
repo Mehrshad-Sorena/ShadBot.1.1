4d5b5bab0344fe337deeff0e46798cba855b63ee
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-10-08 16:54:45 +0330

    Update Runner_BuySecondry.py

--- GeneticLearningRunner/MACD/Runner_BuySecondry.py
+++ GeneticLearningRunner/MACD/Runner_BuySecondry.py
@@ -60,7 +60,7 @@ def Run(number_data_5M):
 	optimizers.symbol = 'XAUUSD_i'
 	optimizers.sigpriority = 'secondry'
 	optimizers.sigtype = 'buy'
-	optimizers.turn = 1000
+	optimizers.turn = 300
 	optimizers.dataset = parameters.elements['dataset_5M'].copy()
 	optimizers.timeframe = '5M'
 
